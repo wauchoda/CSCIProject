f875ca9b5af3d485a50f874663044f6bc72b6a88
Ben MacNeil <airouis.ben@gmail.com>
2015-04-05 16:19:06 -0300

    GUI Files

---
+++ MainWindow.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+//This is the main window for the game
+public class MainWindow extends JFrame implements ActionListener{
+
+  
+      //All the different things in this GUI
+      private JPanel mainPanel;
+      private JPanel invPanel;
+      private JPanel buttonsPanel;
+      private JPanel playerPanel;
+      
+
+   public MainWindow(){
+    
+      
+
+   
+
+
+   }
+
+   //All the responces and stuff
+   public void actionPerformed(ActionEvent e){
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+}
