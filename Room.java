a8ef7bf453ba9c0e96005c866fc75baf194c05fa
Lv20Bard <Airouis.ben@gmail.com>
2015-04-11 16:03:02 -0300

    Room Done for real now
    Seriously

--- Room.java
+++ Room.java
@@ -4,12 +4,13 @@ import java.util.Random;
 //The room class simply generates a room from the list of types
 public class Room {
 
+	private static int roomCount = 0;
 	private String e;
 	private Random rand = new Random();
 	
 	//uses random values and the constructor to make a random room
 	public Room (){
-		
+		roomCount++;
 		int i = rand.nextInt(4)+1;
 		
 		if(i == 1){
